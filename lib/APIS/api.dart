5856b516b6e653af556c873f270ddbe40e31f2db
OnlyKal <onlykal64@gmail.com>
2022-06-19 16:44:34 +0200

    error certificate

--- lib/APIS/api.dart
+++ lib/APIS/api.dart
@@ -1,1 +1,6 @@
-var server_address = 'https://192.168.9.5:3000';
+var serverAddress = 'https://192.168.9.5:3000';
+
+void sendData(endpoint, data) {
+  var token = 'ygugdiufhluidfhuihfiuhdfli';
+  
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
 import './EXPORTS/exports.files.dart';
 
+
+
+
+class PostHttpOverrides extends HttpOverrides{
+  @override
+  HttpClient createHttpClient(context){
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
+  }
+}
+
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  HttpOverrides.global = new PostHttpOverrides();
   runApp(MaterialApp(
     theme: ThemeData(
         colorScheme: ColorScheme.fromSwatch(
