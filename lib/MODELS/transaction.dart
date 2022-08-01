67ee5cf92b0cdb14ff97cffa021c31093b0f910a
mak098 <sergemakasi098.com>
2022-08-01 20:54:41 +0200

    balances

--- lib/MODELS/transaction.dart
+++ lib/MODELS/transaction.dart
@@ -64,13 +64,13 @@ class Transaction {
       "SELECT * FROM transactions where clientId=$id",
     );
   }
-  Future getUsd(id) {
+  Future getUsd(int id) {
     Db db = Db();
     return db.fetch(
       "SELECT (sum (usdDebit)-sum(usdCredit)) as balance FROM transactions where clientId=$id",
     );
   }
-  Future getCdf(id) {
+  Future getCdf(int id) {
     Db db = Db();
     return db.fetch(
       "SELECT (sum(cdfDebit)-sum(cdfCredit)) as balance FROM transactions where clientId=$id",
